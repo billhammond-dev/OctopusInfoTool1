2b016387b4ade4a003d30fcfa7cc6a6788875dad
Bill Hammond <bill@myremoteprogrammer.com>
2020-05-23 07:11:04 -0500

    Added project files

---
+++ tentacular/to_remove/src/App.vue
@@ -0,0 +1,185 @@
+<template>
+  <div id="app">
+  <div class="topbar">
+    <h2>Octopus Deploy Step History Tool</h2>
+  </div> 
+  <div class="url_input_area">
+    <form @submit.prevent="setUrl">
+      <input class="urlinput" type="text" v-model="text" name="url" placeholder="Octopus Server Url">
+      <input class="connect" type="submit" value="connect">
+      <span class="resp_code">Last Response Code: {{reponseCode}}</span>
+    </form>
+  </div>
+  <div class="selection_area">
+    <div class="selector">
+      <select class="dropdown" name="add_name">
+        <option>Select A Project:</option>
+        <option value="value1">option1</option>
+        <option value="value2">option2</option>
+      </select>
+    </div>
+    <div class="selector">
+      <select class="dropdown" name="add_name">
+        <option>Select Release Step:</option>
+        <option value="value1">option1</option>
+        <option value="value2">option2</option>
+      </select>
+    </div>
+  </div>
+  <div class="dvpane"><DataView octopusData="{name1: Test1}"/></div>
+  
+  </div>
+</template>
+<script type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fetch-jsonp/1.0.6/fetch-jsonp.min.js"></script>
+<script>
+import DataView from './components/DataView.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    DataView
+  },
+  data () {
+    return {
+      responseCode: '',
+      url: ''
+    }
+  },
+  methods: {
+    async setUrl (submitEvent) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+      this.url = submitEvent.target.elements.url.value
+      try {
+        const res = await axios.get(this.url, config)
+        this.responseCode = res.data.responseCode
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    scrollbar-width: thin;
+}
+
+
+
+#app {
+  background: #192a56;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.topbar {
+  background-color: #273c75;
+  color: #dcdde1;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  text-align: left;
+  border-bottom: 1px solid #dcdde133;
+}
+
+.url_input_area {
+  padding: 1rem;
+}
+
+.url_input_area .urlinput {
+  font-size: 1.25rem;
+  background: transparent;
+  border: none;
+  outline: none;
+  border-bottom: 2px solid #dcdde1;
+  width: 35rem;
+  color: #dcdde17c;
+  padding-bottom: .25rem;
+}
+
+.url_input_area .connect {
+  margin-left: 1rem;
+  padding: .3rem;
+  font-size: 1.25rem;
+  background: #273c75;
+  border: 1px solid #dcdde136;
+  border-radius: 1rem;
+  outline: none;
+  color: #dcdde1da;
+}
+
+.resp_code {
+  margin-left: 2rem;
+  color: #dcdde167;
+}
+
+.selection_area {
+  padding: 1rem;
+  background-color: #273c75;
+  margin: 1rem;
+  height: 5rem;
+  border: 1px solid #273c75;
+  box-shadow: -1px 1px 5px #00000079;
+  display: flex;
+  align-items: center;
+}
+
+.selector {
+  width: 10rem;
+  margin-left: 10rem;
+}
+.dropdown {
+  font-size: 1.25rem;
+  background: transparent;
+  border: none;
+  outline: none;
+  width: auto;
+  color: #dcdde17c;
+}
+
+.dropdown option {
+  background-color: #273c75;
+}
+
+.dvpane {
+  margin: 1rem;
+  padding: .5rem;
+  flex: 1;
+  overflow: hidden;
+  overflow-y: auto;
+  border: 1px solid #273c75;
+  box-shadow: -1px 1px 5px #00000079;
+  background-color: #273c75;
+}
+
+.dvpane::-webkit-scrollbar {
+  width: 5px;
+}
+/* Track */
+.dvpane::-webkit-scrollbar-track {
+  background: #192a56;
+}
+
+/* Handle */
+.dvpane::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+.dvpane::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+</style>
---
+++ tentacular/to_remove/src/components/DataView.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="pane">
+    Test Data Area SYGDJHGSHJDGSHJDGHJSGDHJSGDHJGSJGDHJGSHDJGSDHGJHDGSJHDGHSJGDDJHGSDHJGSHDGHJSGDHJSGDJHSGDJHSGDJHSGDJHSGDJHSGDJHSGDJHSGDJSGDJHSGDJHSGDJHSGDJHSGDJHSGDJSHDGSJHDGSJHDGJHSGDJHSGDJHSG
+    Octopus Data is: {{octopusData}}
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+    <p>TEST LINE</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataView',
+  props: {
+    octopusData: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.pane {
+  overflow-wrap: break-word;
+  color: #dcdde1;
+}
+</style>
